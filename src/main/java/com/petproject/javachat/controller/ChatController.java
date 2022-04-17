1c110bd8fae75338e90b06131d67ff9099992201
KosivchenkoPetr <nightdemon@mail.ru>
2022-04-17 17:16:39 +0300

    add RabbitMQ

--- src/main/java/com/petproject/javachat/controller/ChatController.java
+++ src/main/java/com/petproject/javachat/controller/ChatController.java
@@ -6,21 +6,35 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class ChatController {
 
     private final MongoTemplate mongoTemplate;
-    public ChatController(MongoTemplate mongoTemplate) {
+    private final SimpUserRegistry simpUserRegistry;
+
+    public ChatController(MongoTemplate mongoTemplate, SimpUserRegistry simpUserRegistry) {
         this.mongoTemplate = mongoTemplate;
-   }
+        this.simpUserRegistry = simpUserRegistry;
+    }
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
         //TODO: Сохранение в сервисе - это ОК?
         mongoTemplate.save(chatMessage);
+        List usersList = this.simpUserRegistry
+                .getUsers()
+                .stream()
+                .map(SimpUser::getName)
+                .collect(Collectors.toList());
+
         return chatMessage;
     }
     @MessageMapping("/chat.addUser")
